d99151a1f748bdca6e2e345dd0f69770339d0e85
Kirill Sumorokov <sumorokov.k@gmail.com>
2018-03-24 14:04:02 +0500

    Allow run local and in docker containers

--- tests/test_pg_server.py
+++ tests/test_pg_server.py
@@ -2,7 +2,6 @@ import psycopg2
 
 
 def test_pg_server(pg_server):
-    pg_params = pg_server['pg_params']
-    with psycopg2.connect(**pg_params) as conn:
+    with psycopg2.connect(**pg_server['params']) as conn:
         with conn.cursor() as cursor:
             cursor.execute('SELECT version();')
